8353e72c840bd1500efa7e533eee823fd8831cbd
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-05 19:56:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -8,9 +8,9 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
-	{  
-		:known_for => "Object Orientation", 
-		:languages => ["Smalltalk", "LISP"] 
+	{
+		:known_for => "Object Orientation",
+		:languages => ["Smalltalk", "LISP"]
 	} 
 end
 
